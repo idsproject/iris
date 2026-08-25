55660855b7f9970ca4e8410f1fcb5ee655ba6618
Andromeda Sawtelle <andromeda.sawtelle@gmail.com>
2026-08-25 10:47:07 -0400

    Seperated tests to another handler

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 GO ?= go
 LINTER ?= golangci-lint
 
-.PHONY: all format build test lint lint-fix deploy dev-spinup
+.PHONY: all format build test lint lint-fix deploy local-spinup local-update
 
 all: build test format lint
 
@@ -23,8 +23,15 @@ lint-fix:
 deploy:
 	echo "Deploying..."
 
-dev-spinup:
+local-spinup:
 	docker network create iris-test
 	docker run -d --name iris-postgres --network iris-test --env-file .env postgres
 	docker build -t iris ./
 	docker run -d --name iris --network iris-test --env-file .env -p "127.0.0.1:8080:8080" iris
+
+local-update:
+	docker build -t iris ./ \
+	&& docker stop iris \
+	&& docker rm iris \
+	&& docker run -d --name iris --network iris-test --env-file .env -p "127.0.0.1:8080:8080" \
+		-v ./test_pdfs:/test_pdfs iris
--- handler/mainHandler.go
+++ handler/mainHandler.go
@@ -3,10 +3,6 @@ package handler
 import (
 	"log/slog"
 	"net/http"
-	"os"
-
-	"github.com/idsproject/iris/aws"
-	"github.com/idsproject/iris/util"
 
 	"github.com/idsproject/iris/internal/data"
 
@@ -34,99 +30,9 @@ func CreateMainHandler(logger *slog.Logger, logsModel *data.LogsModel) *MainHand
 }
 
 func (handler *MainHandler) Routes(router chi.Router) {
-	router.Get("/buckets", handler.HandleBuckets)
 	router.Post("/upload", handler.HandleUpload)
-	router.Post("/testupload", handler.HandleTestToAws)
-	router.Get("/testlist", handler.HandleTestList)
-	router.Post("/testdownload", handler.HandleTestFromAws)
-}
-
-func (handler *MainHandler) HandleBuckets(w http.ResponseWriter, r *http.Request) {
-	// buckets, err := aws.GetBuckets(handler.Logger)
-	// if err != nil {
-	// 	handler.Logger.Error("HandleBuckets/aws/GetBuckets", "err", err)
-	// 	err = util.Error(w, r, http.StatusInternalServerError, "Can't get buckets")
-	// 	if err != nil {
-	// 		handler.Logger.Error("HandleBuckets/util/Error", "err", err)
-	// 	}
-	// 	return
-	// }
-
-	// err = util.Success(w, r, buckets)
-	// if err != nil {
-	// 	handler.Logger.Error("HandleBuckets/util/Success", "err", err)
-	// }
 }
 
 func (handler *MainHandler) HandleUpload(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("uploading")) //nolint:errcheck,gosec
-}
-
-func (handler *MainHandler) HandleTestToAws(w http.ResponseWriter, r *http.Request) {
-	fileName := r.URL.Query().Get("name")
-
-	workingDir, err := os.Getwd()
-	if err != nil {
-		handler.GetLogger().Error("HandleTest/os/Getwd", "err", err)
-		err = util.Error(w, r, http.StatusInternalServerError, "err")
-		if err != nil {
-			handler.GetLogger().Error("HandleTestToAws/util/Error", "err", err)
-		}
-		return
-	}
-
-	filePath := workingDir + "/test_pdfs/" + fileName
-
-	err = aws.UploadArticle(filePath)
-	if err != nil {
-		handler.GetLogger().Error("HandleTest/aws/UploadArticle", "err", err)
-		err = util.Error(w, r, http.StatusInternalServerError, "err")
-		if err != nil {
-			handler.GetLogger().Error("HandleTestToAws/util/Error", "err", err)
-		}
-		return
-	}
-
-	err = util.Success(w, r, "success")
-	if err != nil {
-		handler.GetLogger().Error("HandleTestToAws/util/Success", "err", err)
-	}
-}
-
-func (handler *MainHandler) HandleTestFromAws(w http.ResponseWriter, r *http.Request) {
-	fileName := r.URL.Query().Get("name")
-
-	err := aws.DownloadArticle(fileName)
-	if err != nil {
-		handler.GetLogger().Error("HandleTestFromAws/aws/DownloadArticle", "err", err)
-		err = util.Error(w, r, http.StatusInternalServerError, err)
-		if err != nil {
-			handler.GetLogger().Error("HandleTestFromAws/util/Error", "err", err)
-		}
-		return
-	}
-
-	err = util.Success(w, r, "success")
-	if err != nil {
-		handler.GetLogger().Error("HandleTestFromAws/util/Success", "err", err)
-	}
-}
-
-func (handler *MainHandler) HandleTestList(w http.ResponseWriter, r *http.Request) {
-	// fileName := r.URL.Query().Get("name")
-
-	objects, err := aws.ListObjects()
-	if err != nil {
-		handler.GetLogger().Error("HandleTestList/aws/ListObjects", "err", err)
-		err = util.Error(w, r, http.StatusInternalServerError, "err")
-		if err != nil {
-			handler.GetLogger().Error("HandleTestList/util/Error", "err", err)
-		}
-		return
-	}
-
-	err = util.Success(w, r, objects)
-	if err != nil {
-		handler.GetLogger().Error("HandleTestList/util/Success", "err", err)
-	}
 }
